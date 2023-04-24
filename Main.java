a21d1c701e1a0fe371078de92cf4c45a0236cb7b
weichun <62707493+alextang809@users.noreply.github.com>
2023-04-24 20:29:12 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,29 +1,68 @@
 @FunctionalInterface
-interface Multiplier<T, U> {
-    int multiply(T a, U b);
+interface Adder<T> {
+    T add(T first, T second);
+}
+
+@FunctionalInterface
+interface StringTransformer<T> {
+    T transform(String initialString);
+}
+
+@FunctionalInterface
+interface TypeConverter<T, R> {
+    R convert(T input);
 }
 
 public class Main {
-    // Higher-Order 1: Function accepts a function as an argument
-    private static int execute(Multiplier<Integer, Integer> multiplier){
-        return multiplier.multiply(5, 10);
+    public static <T> T addNumbers(Adder<T> adder, T firstNumber, T secondNumber) {
+        return adder.add(firstNumber, secondNumber);
     }
 
-    // Higher-Order 2: Function returns another function
-    private static Multiplier<Integer, Integer> getMultiplier() {
-        // First-Class 1: Function assigned to a variable
-        Multiplier<Integer, Integer> multiplier =  (a, b) -> a * b;
+    public static <T> T transformString(StringTransformer<T> stringTransformer, String initialString) {
+        return stringTransformer.transform(initialString);
+    }
 
-        // First-Class 3: Function returned from a function
-        return multiplier;
+    public static <T, R> R convertValue(TypeConverter<T, R> converter, T input) {
+        return converter.convert(input);
     }
 
     public static void main(String[] args) {
-        Multiplier<Integer, Integer> multiplier = getMultiplier();
+        Adder<Integer> integerAdder = (a, b) -> a + b;
+        int result1 = addNumbers(integerAdder, 10, 20);
+        System.out.println(result1);
 
-        // First-Class 2: Function passed as an argument
-        int ans = execute(multiplier);
-        
-        System.out.println(ans);
+        Adder<Double> doubleAdder = (a, b) -> a + b;
+        double result2 = addNumbers(doubleAdder, 10.5, 20.5);
+        System.out.println(result2);
+
+        StringTransformer<Integer> stringLength = new StringTransformer<Integer>() {
+            @Override
+            public Integer transform(String inputString) {
+                return inputString.length();
+            }
+        };
+
+        int stringLengthResult = transformString(stringLength, "Hello");
+        System.out.println(stringLengthResult);
+
+        StringTransformer<String> toUpperCase = new StringTransformer<String>() {
+            @Override
+            public String transform(String inputString) {
+                return inputString.toUpperCase();
+            }
+        };
+
+        String upperCasedString = transformString(toUpperCase, "hello");
+        System.out.println(upperCasedString);
+
+        TypeConverter<Integer, String> intToStringConverter = new TypeConverter<Integer, String>() {
+            @Override
+            public String convert(Integer input) {
+                return Integer.toString(input);
+            }
+        };
+
+        String stringValue = convertValue(intToStringConverter, 123);
+        System.out.println(stringValue);
     }
 }
